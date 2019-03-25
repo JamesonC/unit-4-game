d1de36a30f350a92679e286b22547b22262f8a61
Jameson Campbell <jcampbell@flexterapp.com>
2019-03-25 14:22:45 -0500

    completed game logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,52 @@
-var targetNumber = 0;
-var counter = 0; 
+var counter = 0;
 var totalWins = 0;
 var totalLosses = 0;
 
-// var targetNumber = Math.floor(Math.random() * 120) + 19;
-
+$('#targetNumber').text(targetNumber); 
 $(`#totalWins`).text(totalWins);
 $(`#totalLosses`).text(totalLosses);
+$(`#totalScore`).text(0);
 
-var targetNumber = 30;
-$('#targetNumber').text(targetNumber);
+// var numberOptions = [10, 5, 3, 7];
 
-var numberOptions = [10, 5, 3, 7];
+var numberOptions = [Math.floor((Math.random() * 12) + 1), Math.floor((Math.random() * 12) + 1), Math.floor((Math.random() * 12) + 1), Math.floor((Math.random() * 12) + 1)]
 
 for (var i = 0; i < numberOptions.length; i++) {
     var imageCrystal = $(`<img>`);
-
     imageCrystal.addClass("crystal-image");
-    imageCrystal.attr("src", src="http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+    imageCrystal.attr("src", src = "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
     imageCrystal.attr("data-crystalvalue", numberOptions[i]);
     $(`#crystals`).append(" ", imageCrystal);
-};
+};;
 
+$(`.crystal-image`).click(function () {
 
-$(`.crystal-image`).click(function() {
-    counter += 10;
+    var crystalValue = ($(this).attr("data-crystalvalue"));
+    crystalValue = parseInt(crystalValue);
+
+    counter += crystalValue;
+
     $(`#totalScore`).text(counter);
-
     if (counter === targetNumber) {
         alert("You win!");
+        resetGame();
+        totalWins++;
+        $(`#totalWins`).text(totalWins) = totalWins;
+
     } else if (counter >= targetNumber) {
         alert("You lose!");
-    } else (counter <= targetNumber); {
+        resetGame();
+        totalLosses++;
+        $(`#totalLosses`).text(totalLosses) = totalLosses;
+
+    } else(counter <= targetNumber); {
         console.log("Keep playing");
     }
-
 });
+
+function resetGame() {
+    counter = 0;
+    $(`#totalScore`).text(0);
+    targetNumber = Math.floor(Math.random() * 120) + 19;
+    $('#targetNumber').text(targetNumber); 
+}
