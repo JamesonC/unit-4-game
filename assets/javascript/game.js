a52451a23ac11604603296613406bb867cb167d2
Jameson Campbell <jcampbell@flexterapp.com>
2019-03-25 14:55:58 -0500

    completed game logic and rounded images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,12 +2,9 @@ var counter = 0;
 var totalWins = 0;
 var totalLosses = 0;
 
-$('#targetNumber').text(targetNumber); 
 $(`#totalWins`).text(totalWins);
 $(`#totalLosses`).text(totalLosses);
 $(`#totalScore`).text(0);
-
-// var numberOptions = [10, 5, 3, 7];
 
 var numberOptions = [Math.floor((Math.random() * 12) + 1), Math.floor((Math.random() * 12) + 1), Math.floor((Math.random() * 12) + 1), Math.floor((Math.random() * 12) + 1)]
 
@@ -17,7 +14,7 @@ for (var i = 0; i < numberOptions.length; i++) {
     imageCrystal.attr("src", src = "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
     imageCrystal.attr("data-crystalvalue", numberOptions[i]);
     $(`#crystals`).append(" ", imageCrystal);
-};;
+};
 
 $(`.crystal-image`).click(function () {
 
@@ -49,4 +46,4 @@ function resetGame() {
     $(`#totalScore`).text(0);
     targetNumber = Math.floor(Math.random() * 120) + 19;
     $('#targetNumber').text(targetNumber); 
-}
+};
