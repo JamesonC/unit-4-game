d8e7947990398cb39ce4a62b98438044152e1342
Jameson Campbell <jcampbell@flexterapp.com>
2019-03-27 16:56:01 -0500

    background image & crystal images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,7 +11,7 @@ var numberOptions = [Math.floor((Math.random() * 12) + 1), Math.floor((Math.rand
 for (var i = 0; i < numberOptions.length; i++) {
     var imageCrystal = $(`<img>`);
     imageCrystal.addClass("crystal-image");
-    imageCrystal.attr("src", src = "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+    imageCrystal.attr("src", src = "https://images.unsplash.com/photo-1551122087-f99a4ade46ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80");
     imageCrystal.attr("data-crystalvalue", numberOptions[i]);
     $(`#crystals`).append(" ", imageCrystal);
 };
